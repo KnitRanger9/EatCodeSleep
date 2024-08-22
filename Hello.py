0d8ad9f98fac4f9bbb68fa185d54504b68208ade
KnitRanger9 <da24m004@smail.iitm.ac.in>
2024-08-22 12:54:31 +0530

    Create Hello.py "OM NAMAH SHIVAY"

---
+++ Hello.py
@@ -0,0 +1,8 @@
+#starting with a standard hello world program for good omens ;)
+if __name__=="__main__":
+  print("Hello World")
+  print(f"Lets start the {"Python"} journey with me!!") #this was utter nonsense
+# just wasting time
+# dont read, or you might end up losing money
+# still reading??
+# im not stopping you
